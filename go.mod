7d8681038e555378e1b69d4891b47a64ccf74c67
Viet Hung <viethung0823@gmail.com>
2024-08-14 22:59:13 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/viethung0823/MassCodeAlfred
+
+go 1.13
+
+require github.com/deanishe/awgo v0.29.1
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	aw "github.com/deanishe/awgo"
+)
+
+type Folder struct {
+	Id              string  `json:"id"`
+	Name            string  `json:"name"`
+	DefaultLanguage string  `json:"defaultLanguage"`
+	ParentId        *string `json:"parentId"`
+	IsOpen          bool    `json:"isOpen"`
+	IsSystem        bool    `json:"isSystem"`
+	CreatedAt       int64   `json:"createdAt"`
+	UpdatedAt       int64   `json:"updatedAt"`
+	Index           int     `json:"index"`
+}
+
+type Content struct {
+	Label    string `json:"label"`
+	Language string `json:"language"`
+	Value    string `json:"value"`
+}
+
+type Snippet struct {
+	Name        string    `json:"name"`
+	Id          string    `json:"id"`
+	Description string    `json:"description"`
+	IsDeleted   bool      `json:"isDeleted"`
+	Folder      *Folder   `json:"folder"`
+	Content     []Content `json:"content"`
+	IsFavorites bool      `json:"isFavorites"`
+	FolderId    string    `json:"folderId"`
+	TagsIds     []string  `json:"tagsIds"`
+	CreatedAt   int64     `json:"createdAt"`
+	UpdatedAt   int64     `json:"updatedAt"`
+}
+
+var wf = aw.New()
+
+func main() {
+	log.Print(os.Args)
+	searchType := os.Args[1]
+	query := os.Args[2]
+	if searchType == "snippets" {
+		searchSnippets()
+	} else if searchType == "folders" {
+		searchFolders()
+	}
+
+	if query != "" {
+		wf.Filter(query)
+	}
+
+	wf.SendFeedback()
+}
+
+func searchSnippets() {
+	resp, err := http.Get("http://localhost:3033/snippets")
+	if err != nil {
+		log.Fatalf("Failed to fetch data: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var data []Snippet
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		log.Fatalf("Failed to decode JSON: %v", err)
+	}
+
+	for _, snippet := range data {
+		if !snippet.IsDeleted {
+			title := snippet.Name
+			subtitle := "Inbox"
+			if snippet.Folder != nil {
+				subtitle = snippet.Folder.Name
+			}
+			urlScheme := "masscode://snippets/" + snippet.Id
+
+			item := wf.NewItem(title).
+				Subtitle(subtitle).
+				UID(snippet.Id).
+				Var("description", snippet.Description).
+				Arg(snippet.Content[0].Value).
+				Valid(true)
+
+			item.Cmd().Arg(urlScheme).Subtitle("Open in MassCode")
+			item.Opt().Subtitle("View snippet")
+		}
+	}
+}
+
+func searchFolders() {
+	// wait for masscode update url scheme for folders
+	resp, err := http.Get("http://localhost:3033/folders")
+	if err != nil {
+		log.Fatalf("Failed to fetch data: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var data []Folder
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		log.Fatalf("Failed to decode JSON: %v", err)
+	}
+
+	for _, folder := range data {
+		wf.NewItem(folder.Name).
+			Subtitle(folder.DefaultLanguage).
+			UID(folder.Id).
+			Valid(true)
+	}
+}
